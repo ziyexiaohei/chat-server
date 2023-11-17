c018e9c6a73f53ae3684b0ec1c310aa9120594bf
MohaElder <calen0909@hotmail.com>
2023-11-17 10:24:42 -0800

    feat(chat): Implement Retrieve and Save history

--- ChatServer.java
+++ ChatServer.java
@@ -1,10 +1,13 @@
 import java.io.IOException;
 import java.net.URI;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 
 class ChatHandler implements URLHandler {
   String chatHistory = "";
-  public String handleRequest(URI url) {
 
+  public String handleRequest(URI url) {
 
     // expect /chat?user=<name>&message=<string>
     if (url.getPath().equals("/chat")) {
@@ -15,18 +18,55 @@ class ChatHandler implements URLHandler {
         String user = shouldBeUser[1];
         String message = shouldBeMessage[1];
         this.chatHistory += user + ": " + message + "\n\n";
-        return this.chatHistory; 
-      }
-      else {
+        return this.chatHistory;
+      } else {
         return "Invalid parameters: " + String.join("&", params);
       }
     }
+    // expect /retrieve-history?file=<name>
+    else if (url.getPath().equals("/retrieve-history")) {
+      String[] params = url.getQuery().split("&");
+      String[] shouldBeFile = params[0].split("=");
+      if (shouldBeFile[0].equals("file")) {
+        String fileName = shouldBeFile[1];
+        // String fileName = shouldBeFileName[0]; // bug4: should be shouldBeFile[1]
+        ChatHistoryReader reader = new ChatHistoryReader();
+        try {
+          String[] contents = reader.readFileAsArray("chathistory/" + fileName);
+          for (String line : contents) {
+            this.chatHistory += line + "\n\n";
+          }
+        } catch (IOException e) {
+          System.err.println("Error reading file: " + e.getMessage());
+        }
+      }
+      return this.chatHistory;
+    }
+    // expect /save?name=<name>
+    else if (url.getPath().equals("/save")) {
+      String[] params = url.getQuery().split("&");
+      String[] shouldBeFileName = params[0].split("=");
+      if (shouldBeFileName[0].equals("name")) {
+        File directory = new File("chathistory");
+        File file = new File(directory, shouldBeFileName[1]);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+          writer.write(this.chatHistory);
+          return "Data written to " + shouldBeFileName[1] + "in 'chat-history' folder.";
+        } catch (IOException e) {
+          e.printStackTrace();
+          return "Error: Something wrong happen during file save, check StackTrace";
+        }
+      }
+    }
+
     return "404 Not Found";
   }
 }
 
-class ChatMain {
-  public static void main(String[] args) throws IOException {
-    Server.start(4000, new ChatHandler());
+class ChatServer {
+    public static void main(String[] args) throws IOException {
+      int port = Integer.parseInt(args[0]);
+      Server.start(port, new ChatHandler());
+    }
   }
-}
