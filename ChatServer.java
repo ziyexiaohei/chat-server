6ca3034d1c1c56d608b7cef0b578febe74f4a015
MohaElder <calen0909@hotmail.com>
2023-11-16 17:59:39 -0800

    feat(chat): merge branch

--- ChatServer.java
+++ ChatServer.java
@@ -1,10 +1,10 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.Arrays;
 
 class ChatHandler implements URLHandler {
   String chatHistory = "";
   public String handleRequest(URI url) {
-
 
     // expect /chat?user=<name>&message=<string>
     if (url.getPath().equals("/chat")) {
@@ -21,11 +21,42 @@ class ChatHandler implements URLHandler {
         return "Invalid parameters: " + String.join("&", params);
       }
     }
+    else if(url.getPath().equals("/semantic-analysis")) {
+      String[] params = url.getQuery().split("&");
+      String[] shouldBeUser = params[0].split("=");
+      String matchingMessages = "";
+      if (shouldBeUser[0].equals("user")) {
+         for(String line : this.chatHistory.split("\n\n")) {
+          String analysis = "";
+           if (line.contains(shouldBeUser[1])) {
+            int numberOfExclamationMarks = 0;
+            for(int character : line.codePoints().toArray()) {
+              if(character == (int)'!') {
+                numberOfExclamationMarks += 1;
+              }
+              if(new String(Character.toChars(character)).equals("😂")) {
+                analysis += " This message has a LOL vibe.";
+              }
+              if(new String(Character.toChars(character)).equals("🥹")) {
+                analysis += " This message has a awwww vibe.";
+              }
+            }
+            if (numberOfExclamationMarks > 2){
+              analysis += " This message ends forcefully.";
+            }
+            matchingMessages += line + analysis + "\n\n";
+           }
+         }
+
+         return matchingMessages;
+
+      }
+    }
     return "404 Not Found";
   }
 }
 
-class ChatMain {
+class ChatServer {
   public static void main(String[] args) throws IOException {
     Server.start(4000, new ChatHandler());
   }
--- HandlerTests.java
+++ HandlerTests.java
@@ -16,7 +16,7 @@ public class HandlerTests {
   public void handleRequest2() throws Exception {
     ChatHandler h = new ChatHandler();
     // NOTE: %20 is the way to put a space in the parameters of a URL
-    String url = "http://localhost:4000/chat?name=edwin&message=happy%20friday!";
+    String url = "http://localhost:4000/chat?user=edwin&message=happy%20friday!";
     URI input = new URI(url);
     String expected = "edwin: happy friday!\n\n";
     assertEquals(expected, h.handleRequest(input));
@@ -32,5 +32,21 @@ public class HandlerTests {
     String expected = "onat: good luck\n\nedwin: with your demo!\n\n";
     h.handleRequest(input1);
     assertEquals(expected, h.handleRequest(input2));
+  }
+
+  @Test
+  public void handleRequestSemanticAnalysis() throws Exception {
+    ChatHandler h = new ChatHandler();
+    String url1 = "http://localhost:4000/chat?user=onat&message=😂";
+    // String url2 = "http://localhost:4000/chat?user=onat&message=doggy🥹!!!";
+    String url3 = "http://localhost:4000/semantic-analysis?user=onat";
+    URI input1 = new URI(url1);
+    // URI input2 = new URI(url2);
+    URI input3 = new URI(url3);
+    String expected = "onat: 😂 This message has a LOL vibe.\n\n";
+
+    h.handleRequest(input1);
+    // h.handleRequest(input2);
+    assertEquals(expected, h.handleRequest(input3));
   }
 }
